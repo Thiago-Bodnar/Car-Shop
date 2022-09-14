ab4eb2afe7f211cd5801199ccbe2c1d254e897a2
Thiago <bodnarthiago@gmail.com>
2022-09-14 16:00:34 -0300

    feat: model Car criado

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 import { IVehicle } from './IVehicle';
 
-const carZodSchema = z.object({
+export const carZodSchema = z.object({
   doorsQty: z.number().gte(2).lte(4),
   seatsQty: z.number().gte(2).lte(7),
 });
