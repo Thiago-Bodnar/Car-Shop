f60688830babdf14e502ce04774de60f89d1a277
Thiago <bodnarthiago@gmail.com>
2022-09-14 15:28:08 -0300

    feat: ICar implementada

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { IVehicle } from './IVehicle';
+
+const carZodSchema = z.object({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+type CarSchema = z.infer<typeof carZodSchema>;
+
+export type ICar = IVehicle & CarSchema;
