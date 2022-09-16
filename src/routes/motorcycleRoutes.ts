8f551f4d2f461d74f73849e86bcb99f4fa79336c
Thiago <bodnarthiago@gmail.com>
2022-09-16 10:08:42 -0300

    feat: implementa MotorcycleRoutes

--- src/routes/motorcycleRoutes.ts
+++ src/routes/motorcycleRoutes.ts
@@ -1,3 +1,14 @@
-// import { Router } from "express";
-// import MotorcycleService from "../services/MotorcycleService";
-// import MotorcycleController from "../controllers/MotorcycleController";
+import { Router } from 'express';
+import MotorcycleController from '../controllers/MotorcycleController';
+import MotorcycleModel from '../models/MotorcycleModel';
+import MotorcycleService from '../services/MotorcycleService';
+
+const route = Router();
+
+const motorcycleModel = new MotorcycleModel();
+const motorcycleService = new MotorcycleService(motorcycleModel);
+const motorcycleController = new MotorcycleController(motorcycleService);
+
+route.post('/', (req, res) => motorcycleController.create(req, res));
+
+export default route;
