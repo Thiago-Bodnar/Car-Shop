48dad54a2126a94305a526440ff9760c1efa2e4b
Thiago <bodnarthiago@gmail.com>
2022-09-16 09:57:04 -0300

    feat: implementa MotorcycleController

---
+++ src/controllers/MotorcycleController.ts
@@ -0,0 +1,4 @@
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import VehicleController from './VehicleController';
+
+export default class MotorcycleController extends VehicleController<IMotorcycle> { }
