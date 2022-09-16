2c3b3f707f023906857bd3975c9e7fde1489416b
Thiago <bodnarthiago@gmail.com>
2022-09-16 09:48:55 -0300

    refactor: agora a classe CarController extende a classe Generica VehicleController

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,42 +1,4 @@
-import { Request, Response } from 'express';
 import { ICar } from '../interfaces/ICar';
-import { IService } from '../interfaces/IService';
+import VehicleController from './VehicleController';
 
-export default class CarController {
-  constructor(private _service: IService<ICar>) { }
-
-  public async create(req: Request, res: Response<ICar>) {
-    const car = req.body;
-    const results = await this._service.create(car);
-    return res.status(201).json(results);
-  }
-
-  public async read(_req: Request, res: Response<ICar[]>) {
-    const results = await this._service.read();
-    return res.status(200).json(results);
-  }
-
-  public async readOne(req: Request, res: Response<ICar | null>) {
-    const { id } = req.params;
-    const results = await this._service.readOne(id);
-
-    return res.status(200).json(results);
-  }
-
-  public async update(req: Request, res: Response<ICar | null>) {
-    const { id } = req.params;
-    const data = req.body;
-
-    const results = await this._service.update(id, data);
-
-    return res.status(200).json(results);
-  }
-
-  public async delete(req: Request, res: Response<ICar | null>) {
-    const { id } = req.params;
-
-    await this._service.delete(id);
-
-    return res.sendStatus(204);
-  }
-}
+export default class CarController extends VehicleController<ICar> {}
